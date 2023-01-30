486c73a650538a2da4ea6ee92fbc11d101e11fb4
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-01-30 14:23:55 -0600

    First Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["restaurant"]
+
+
+@app.route("/")
+def index():
+    collection = db["users"]
+    data = collection.find()
+    return render_template("src\index.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
