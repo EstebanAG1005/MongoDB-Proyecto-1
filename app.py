2340ae553f0a67f77da3cf51d8f6f7006fe91df5
unknown <josemarianoreyesh02@gmail.com>
2023-01-30 20:07:36 -0600

    cambios de form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
+import random
 
 app = Flask(__name__)
 
@@ -7,17 +8,59 @@ client = MongoClient("mongodb://localhost:27017")
 db = client["inventario_empresa"]
 
 
-# INDEX
-@app.route("/")
+@app.route("/")  # INDEX
 def index():
     collection = db["productos"]
     productos = collection.find()
     return render_template("index.html", productos=productos)
 
-# SEARCH BAR
+
+@app.route("/productos")  # Productos
+def productos():
+    collection = db["productos"]
+    productos = collection.find()
+    return render_template("productos.html", productos=productos)
 
 
-@app.route('/search')
+@app.route("/categorias")  # Categorias
+def categorias():
+    collection = db["categorias"]
+    categorias = collection.find()
+    return render_template("categorias.html", categorias=categorias)
+
+
+@app.route('/add_producto', methods=['GET', 'POST'])  # ADD PRODUCTOS
+def add_producto():
+
+    productos = db["productos"]
+    proveedores = db["proveedores"]
+    categorias = db["categorias"]
+
+    proveedores_list = proveedores.find()
+    categorias_list = categorias.find()
+
+    random_id = random.randint(0, 100000000)
+
+    while productos.find_one({'id': random_id}) is not None:
+        random_id = random.randint(0, 100000000)
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        price = request.form.get('price')
+        stock = request.form.get('stock')
+        provider = request.form.get('provider')
+        category = request.form.get('category')
+        thumbnail = request.form.get('thumbnail')
+        inserts = {'id': random_id, 'title': title, 'description': description, 'price': price,
+                   'stock': stock, 'provider': provider, 'category': category, 'thumbnail': thumbnail}
+        productos.insert_one(inserts)
+        flash('Producto añadido con éxito!')
+        return redirect(url_for('add_producto'))
+    return render_template('form_new_product.html', proveedores=proveedores_list, categorias=categorias_list)
+
+
+@app.route('/search')  # SEARCH BAR
 def search():
     collection = db["productos"]
     query = request.args.get('q')
