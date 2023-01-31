47e311bb4dc8de400b011533c89faec65dde0499
unknown <josemarianoreyesh02@gmail.com>
2023-01-30 18:10:36 -0600

    search bar listo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,11 +7,26 @@ client = MongoClient("mongodb://localhost:27017")
 db = client["inventario_empresa"]
 
 
+# INDEX
 @app.route("/")
 def index():
     collection = db["productos"]
     productos = collection.find()
     return render_template("index.html", productos=productos)
+
+# SEARCH BAR
+
+
+@app.route('/search')
+def search():
+    collection = db["productos"]
+    query = request.args.get('q')
+    if query:
+        results = collection.find(
+            {'title': {'$regex': query, '$options': 'i'}})
+    else:
+        results = []
+    return render_template('search_results.html', query=query, results=results)
 
 
 if __name__ == "__main__":
